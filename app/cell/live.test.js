bb5a1cac011f35641b540e1ce2e1e8cef3236a69
Carlos Aliaga <carlosaligom@gmai.com>
2023-04-15 16:50:53 +0200

    Add live method to cell and test it

---
+++ app/cell/live.test.js
@@ -0,0 +1,15 @@
+import Cell from "./cell";
+describe("Given a class called Cell", () => {
+  describe("When it gets implemented the method die", () => {
+    test("then it should change his property isAlive to false", () => {
+      const newCell = new Cell(0, 0);
+      newCell.die();
+      newCell.live();
+      const expectedResult = true;
+
+      const result = newCell.isAlive;
+
+      expect(expectedResult).toBe(result);
+    });
+  });
+});
