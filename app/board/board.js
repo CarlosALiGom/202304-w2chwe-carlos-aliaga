76b0245fdb9f20f5b1e11a58c22e52d601f287ae
Carlos Aliaga <carlosaligom@gmai.com>
2023-04-15 17:37:26 +0200

    Create world class

--- app/board/board.js
+++ app/board/board.js
@@ -1,23 +1,23 @@
 import Cell from "../cell/cell";
-const createBoard = (totalRow, totalCol) => {
-  const table = [];
 
-  for (let rowIndex = -1; rowIndex < totalRow; rowIndex++) {
-    const row = [];
+class World {
+  table;
 
-    for (let colIndex = -1; colIndex < totalCol; colIndex++) {
-      row.push(new Cell(colIndex, rowIndex));
+  createBoard(totalRow, totalCol) {
+    const table = [];
+
+    for (let rowIndex = -1; rowIndex < totalRow; rowIndex++) {
+      const row = [];
+
+      for (let colIndex = -1; colIndex < totalCol; colIndex++) {
+        row.push(new Cell(colIndex, rowIndex));
+      }
+
+      table.push(row);
     }
 
-    table.push(row);
+    return table;
   }
+}
 
-  return table;
-};
-
-const board = createBoard(2, 2);
-
-board[0][0].die();
-console.table(board);
-
-export default createBoard;
+export default World;
--- app/cell/die.test.js
+++ app/cell/die.test.js
@@ -1,7 +1,7 @@
 import Cell from "./cell";
 describe("Given a class called Cell", () => {
   describe("When it gets implemented the method die", () => {
-    test("then it should change his property isAlive to false", () => {
+    test("Then it should change his property isAlive to false", () => {
       const newCell = new Cell(0, 0);
       newCell.die();
       const expectedResult = false;
