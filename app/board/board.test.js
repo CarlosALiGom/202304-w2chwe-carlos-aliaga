2758eda86c99a1756ea3771f7c45547557eee3b2
Carlos Aliaga <carlosaligom@gmai.com>
2023-04-15 16:00:26 +0200

    Test createBoard case equal array

---
+++ app/board/board.test.js
@@ -0,0 +1,21 @@
+import Cell from "../cell/cell";
+import createBoard from "./board";
+
+describe("Given a function called createBoard", () => {
+  describe("When it recieve (2,2)", () => {
+    test("Then it should return a 3x3 bidimensional array", () => {
+      const rowNumber = 2;
+      const colNumber = 2;
+
+      const expectedResult = [
+        [new Cell(-1, -1), new Cell(0, -1), new Cell(1, -1)],
+        [new Cell(-1, 0), new Cell(0, 0), new Cell(1, 0)],
+        [new Cell(-1, 1), new Cell(0, 1), new Cell(1, 1)],
+      ];
+
+      const result = createBoard(rowNumber, colNumber);
+
+      expect(expectedResult).toEqual(result);
+    });
+  });
+});
